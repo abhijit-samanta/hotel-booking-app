f676e7f66cd9d0724863d74ed5479150cb8f29bc
abhijit-samanta <samantaabhijit134@gmail.com>
2024-02-24 11:25:03 +0530

    add css and js

--- js/script.js
+++ js/script.js
@@ -6,11 +6,18 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscrool = () =>{
 	navbar.classList.remove('active');
 }
+
+document.querySelectorAll('.contact .row .faq .box h3').forEach(faqBox=>{
+   faqBox.onclick = () =>{
+     faqBox.parentElement.classList.toggle('active');
+   }
+});
 // come from swiper js navigation
 var swiper = new Swiper(".home-slider", {
 	loop:true,
 	// overfloe effect
 	effect: "coverflow",
+	spaceBetween: 30,
     grabCursor: true,
     coverflowEffect: {
         rotate: 50,
@@ -24,3 +31,44 @@ var swiper = new Swiper(".home-slider", {
         prevEl: ".swiper-button-prev",
       },
     });
+
+var swiper = new Swiper(".gallery-slider", {
+	loop:true,
+	// overfloe effect
+	effect: "coverflow",
+	slidesPerView: "auto",
+	centeredSlides: true,
+    grabCursor: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+
+
+var swiper = new Swiper(".reviews-slider", {
+	loop:true,
+	slidesPerView: "auto",
+    grabCursor: true,
+    spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 1,
+        },
+        991: {
+          slidesPerView: 2,
+        },
+    },
+
+
+    });
+ 
